735e07efec0d96f5ae3895c4c499d4ccac4e3264
José Ángel <ghettoxx2@gmail.com>
2020-04-13 20:15:37 -0500

    Create Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 22:49:38 2020
+
+@author: 52971
+"""
+
+'''
+ Se requiere determinar el costo que tendrá realizar una llamada telefónica 
+ con base en el tiempo que dura la llamada y en el costo por minuto. 
+ '''
+llamada=  int (input('ingrese el tiempo en minutos que dura la llamada:\n'))
+costo= int (input('ingresa el costo por minutode llamada:\n'))
+print ('el costo de la llamada  es´')
+valortotal= (print (llamada*costo))
+print ('Pesos')
+
+ 
+
