74dfff81c4fa2c9fae01b17b49153a7940e7ee4e
José Ángel <ghettoxx2@gmail.com>
2020-04-13 20:14:13 -0500

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 17:22:01 2020
+
+@author: 52971
+"""
+
+'''
+La compañía de autobuses "La curva loca” 
+requiere determinar el costo que tendrá el boleto de un viaje sencillo, 
+esto basado en los kilómetros por recorrer y en el costo por kilómetro. 
+'''
+km= int (input('ingresa los kilometros a recorrer:'))
+costo= int (input('ingresa el costo por km:'))
+print ('el valor total es´')
+valortotal= (print (km*costo))
+
+
+
+
